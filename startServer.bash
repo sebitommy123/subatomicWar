31da278a03809b232bb8f6567ca5d4ca93286a6c
sebitommy123 <sebi.szafir@hotmail.com>
2022-01-05 21:26:54 -0300

    lowered interval

--- startServer.bash
+++ startServer.bash
@@ -16,4 +16,4 @@ docker run -d \
   -e REPO_USER=sebitommy123 \
   -e REPO_PASS=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 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower ghcr.io/sebitommy123/commonwealth:latest --debug
+  containrrr/watchtower ghcr.io/sebitommy123/commonwealth:latest --debug --interval=10
