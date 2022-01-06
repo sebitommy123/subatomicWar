7088d958f6f39cda4cf3645786eebc27909bd6b8
sebitommy123 <sebi.szafir@hotmail.com>
2022-01-05 21:22:48 -0300

    altered title

--- startServer.bash
+++ startServer.bash
@@ -1,4 +1,15 @@
+systemctl start docker
+
+export CR_PAT=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
+
+git config --global user.name 'sebitommy123'
+git config --global user.email 'sebi.szafir@hotmail.com'
+
+echo $CR_PAT | docker login ghcr.io -u sebitommy123 --password-stdin
+
 docker pull ghcr.io/sebitommy123/commonwealth:latest
+
+docker run -p3002:3002 -d ghcr.io/sebitommy123/commonwealth:latest
 
 docker run -d \
   --name watchtower \
