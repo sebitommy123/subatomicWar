96a961c91e7c81a27fefb3775db3b91a78db85b6
sebitommy123 <sebi.szafir@hotmail.com>
2022-01-05 23:45:20 -0300

    fix

--- startServer.bash
+++ startServer.bash
@@ -1,19 +1,23 @@
 systemctl start docker
 
-export CR_PAT=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
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 
 git config --global user.name 'sebitommy123'
 git config --global user.email 'sebi.szafir@hotmail.com'
 
-echo $CR_PAT | docker login ghcr.io -u sebitommy123 --password-stdin
+echo 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 | docker login ghcr.io -u sebitommy123 --password-stdin
 
 docker pull ghcr.io/sebitommy123/commonwealth:latest
 
-docker run -p3002:3002 -d ghcr.io/sebitommy123/commonwealth:latest
+docker run -p3002:3002 -t commonwealth -d ghcr.io/sebitommy123/commonwealth:latest
 
 docker run -d \
   --name watchtower \
   -e REPO_USER=sebitommy123 \
   -e REPO_PASS=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 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower ghcr.io/sebitommy123/commonwealth:latest --debug --interval=10
+  containrrr/watchtower commonwealth --debug --interval=10
+
+docker run -d --name watchtower -e REPO_USER=sebitommy123 -e REPO_PASS=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 -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower ghcr.io/sebitommy123/commonwealth --debug --interval=10
