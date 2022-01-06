f9ecfa86373456ae0fffdf5653ea49ecc6bc6785
sebitommy123 <sebi.szafir@hotmail.com>
2022-01-05 23:53:41 -0300

    fixed startServer.bash

--- startServer.bash
+++ startServer.bash
@@ -20,4 +20,3 @@ docker run -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   containrrr/watchtower commonwealth --debug --interval=10
 
-docker run -d --name watchtower -e REPO_USER=sebitommy123 -e REPO_PASS=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 -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower commonwealth --debug --interval=10
