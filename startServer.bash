adacd7e26c8d942edb22b89f8190104e0b968683
sebitommy123 <sebi.szafir@hotmail.com>
2022-01-12 14:12:23 +0100

    Server-ready

--- startServer.bash
+++ startServer.bash
@@ -1,22 +1,23 @@
-systemctl start docker
+echo "Stay alert for (y/n) prompts)";
 
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
+yum update -y;
 
-git config --global user.name 'sebitommy123'
-git config --global user.email 'sebi.szafir@hotmail.com'
+amazon-linux-extras install docker;
 
-echo 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 | docker login ghcr.io -u sebitommy123 --password-stdin
+systemctl start docker;
 
-docker pull ghcr.io/sebitommy123/commonwealth:latest
+docker stop $(docker ps -aq);
+docker rm $(docker ps -aq);
+docker rmi $(docker images -q);
 
-docker run -p3002:3002 --name commonwealth -d ghcr.io/sebitommy123/commonwealth:latest
+git config --global user.name 'sebitommy123';
+git config --global user.email 'sebi.szafir@hotmail.com';
 
-docker run -d \
-  --name watchtower \
-  -e REPO_USER=sebitommy123 \
-  -e REPO_PASS=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 \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower commonwealth --debug --interval=10
+echo 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 | docker login ghcr.io -u sebitommy123 --password-stdin;
+
+docker pull ghcr.io/sebitommy123/commonwealth:latest;
+
+docker run -p3002:3002 --name commonwealth -d ghcr.io/sebitommy123/commonwealth:latest;
+
+docker run -d --name watchtower -e REPO_USER=sebitommy123 -e REPO_PASS=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 -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower commonwealth --debug --interval=10;
 
